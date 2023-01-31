bf03642d170d57ae6cf35f2a5a949fb838af2a16
Marian Abuhazi <ma4107@columbia.edu>
2023-01-31 10:25:30 -0500

    Removed unnecessary files, and redirections in App.js :sparkles:

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ const App = () => {
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="uni-news" element={<Section id="uni-news" header="UNIVERSITY NEWS" articles={university_news_articles} next="City News" link="/city-news" key={0} />} />
-        <Route path="city-news" element={<Section id="city-news" header="CITY NEWS" articles={city_news_articles} next="Opinion" link="/opinion" key={1} />} />
-        <Route path="opinion" element={<Section id="opinion" header="OPINION" articles={opinion_articles} next="A&E" link="/a&e" key={2} />} />
-        <Route path="a&e" element={<Section id="a&e" header="A&E" articles={ae_articles} next="Sports" link="/sports" key={3} />} />
-        <Route path="sports" element={<Section id="sports" header="SPORTS" articles={sports_articles} next="Home" link="/" key={4} />} />
+        <Route path="uni-news" element={<Section id="uni-news" header="UNIVERSITY NEWS" articles={university_news_articles}/>} />
+        <Route path="city-news" element={<Section id="city-news" header="CITY NEWS" articles={city_news_articles} />} />
+        <Route path="opinion" element={<Section id="opinion" header="OPINION" articles={opinion_articles} />} />
+        <Route path="a&e" element={<Section id="a&e" header="A&E" articles={ae_articles} />} />
+        <Route path="sports" element={<Section id="sports" header="SPORTS" articles={sports_articles} />} />
       </Routes>
     </BrowserRouter>
   );
