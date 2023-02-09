fb881eb407ac712c179655d6186a85a838aac340
Rachel Michelson <rachelmichelson@Rachels-MacBook-Air.local>
2023-02-09 14:09:56 -0500

    done except background

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,22 +3,54 @@ import Credits from './Credits';
 import Letter from '../containers/LetterEditor';
 import NavBar from '../components/NavBar';
 import styled from 'styled-components';
+import './Home.css'
 import MobileNavBar from '../components/MobileNavBar';
+import bhm from "./BHM_digital.png"
 
-const HomeContainer = styled.div`
-    height: 100%;
-    width:100%;
-    background: linear-gradient(#5F193E, #3F132e, #1D132b);
-`;
+/*
+const Home = () => {
+
+
+   return (
+       <HomeContainer>
+           <SpecLogo></SpecLogo>
+           <Line></Line>
+           <TopWrapper>
+           <Photo></Photo>
+               <Title>BLACK HISTORY MONTH</Title>
+           </TopWrapper>
+           {/*
+           <NavBar/>
+           <MobileNavBar/>
+           <Letter/>
+   <Credits/>
+       </HomeContainer>
+   );
+};*/
+
 
 const Home = () => {
 
-    return (
-        <HomeContainer>
-            <Letter/>
-            <Credits/>
-        </HomeContainer>
-    );
+
+   return (
+       <div className = "home-container">
+           <div className='background'>
+          
+           <div className = "spec-logo">
+           <img src = "https://spec-imagehosting.s3.amazonaws.com/CDSwhitemasthead.png" alt = "Columbia Spectator logo" width = "24.375%" height = "auto"></img>
+           </div>
+           <div className = "line"></div>
+           <div className = "wrapper">
+           <div className = "photo"><img src = {bhm} alt = "black history month graphic" width = "100%"></img></div>
+           <div className = "title">BLACK HISTORY<br></br>MONTH</div>
+           </div>
+
+
+               </div>
+           </div>
+   );
 };
 
+
 export default Home;
+
--- src/containers/Section.js
+++ src/containers/Section.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const SectionWrapper = styled.div`
     height: 100vh;
     width: 100vw;
-    background: linear-gradient(#5F193E, #3F132e, #1D132b); 
+    background: linear-gradient(180deg, #661949 0%, #10071D 100%), #3F132E;
 `;
 
 const Section = () => {
