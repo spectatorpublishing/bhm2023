9ac76a1ed73eacffe2bd43a4845b26b7b7124baa
Darien Moment <dmoment18@gmail.com>
2023-02-12 13:22:27 -0500

    Updated Gradient

--- src/containers/Section.js
+++ src/containers/Section.js
@@ -5,8 +5,7 @@ import NavBar from '../components/NavBar';
 const SectionWrapper = styled.div`
      height: 100vh;
      width: 100vw;
-     background: linear-gradient(#5F193E, #3F132e, #1D132b); 
-     background: linear-gradient(180deg, #661949 0%, #10071D 100%), #3F132E;
+     background: linear-gradient(180deg, #945F80 0%, #10071D 100%), #3F132E;
 `;
 
 const Section = () => {
