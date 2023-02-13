7995353a655c15c8670a7aac41ec771f2c8ef628
Caroline <caroline.haoud@columbia.edu>
2023-02-12 23:57:02 -0500

    she's a little wonky but all of the components are there

--- src/containers/Section.js
+++ src/containers/Section.js
@@ -72,6 +72,13 @@ const LittleRectangle1 = styled.div`
     z-index: 2;
 `;
 
+const View1 = styled.image`
+    max-width:100%;
+    height:auto;
+    z-index: 100;
+    margin-right: 700px;
+`;
+
 const Rectangle2 = styled(Rectangle)`
     background-color: #5F415B;
     height: 325px;
@@ -105,8 +112,8 @@ const ArticleInfo1= styled.div`
     text-transform: capitalize;
     font-weight: 600;
     position: relative;
-    margin-top: 100px;
     z-index: 10;
+    margin-top: 100px;
 `;
 const ArticleInfoTitle1 = styled(ArticleInfo1)`
     font-size: 40px;
@@ -116,27 +123,38 @@ const ArticleInfoAuthor1 = styled(ArticleInfo1)`
     font-size: 20px;
 `;
 
+const View2 = styled.image`
+    margin-left: 700px;
+    max-width:100%;
+    height:auto;
+    z-index: 100;
+    position: absolute;
+`;
+
 const ArticleInfo2= styled.div`
     text-align: left;
     color: white;
     font-family: 'EB Garamond';
     font-weight: 600;
     text-transform: capitalize;
+    
     height: 72px;
     width: 393px;
     left: 0px;
     top: 40px;
     z-index: 10;
-    margin-top: 50px;
     margin-right: 300px;
+    margin-top: 50px;
 
 `;
 const ArticleInfoTitle2 = styled(ArticleInfo2)`
     font-size: 40px;
+    z-index: 10;
 `;
 const ArticleInfoAuthor2 = styled(ArticleInfo2)`
     color: white;
     font-size: 20px;
+    z-index: 10;
 `;
 const Section = () => {
 
@@ -145,22 +163,35 @@ const Section = () => {
             <NavBar isSection/>
             <HeaderDiv><Header>UNIVERSITY</Header></HeaderDiv>
             <AllRectangles>
-            <Rectangle1><ArticleInfo1><ArticleInfoTitle1>This Article's Title
-                </ArticleInfoTitle1><ArticleInfoAuthor1>By Some Person's Name</ArticleInfoAuthor1></ArticleInfo1><LittleRectangle1/></Rectangle1>
+            <ArticleInfo1><ArticleInfoTitle1>This Article's Title
+                </ArticleInfoTitle1><ArticleInfoAuthor1>By Some Person's Name</ArticleInfoAuthor1></ArticleInfo1>
+                <View1>
+                    <img src="https://www.history.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTk0OTA1OTY4ODg1MTE0Mzky/hero-crop-gettyimages-459534214.jpg" alt="Dr. King"></img>
+                    </View1>
+            <Rectangle1><LittleRectangle1/></Rectangle1>
 
                 <ArticleInfo2><ArticleInfoTitle2>This Article's Title
                 </ArticleInfoTitle2><ArticleInfoAuthor2>By Some Person's Name</ArticleInfoAuthor2></ArticleInfo2>
-                <Rectangle2><LittleRectangle2/></Rectangle2>
+                <View2>
+                    <img src="https://www.history.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTk0OTA1OTY4ODg1MTE0Mzky/hero-crop-gettyimages-459534214.jpg" alt="Dr. King"></img>
+                </View2>
+                <Rectangle2><View2/><LittleRectangle2/></Rectangle2>
 
 
-            <Rectangle1><ArticleInfo1><ArticleInfoTitle1>This Article's Title
-                </ArticleInfoTitle1><ArticleInfoAuthor1>By Some Person's Name</ArticleInfoAuthor1></ArticleInfo1><LittleRectangle1/></Rectangle1>
+                <ArticleInfo1><ArticleInfoTitle1>This Article's Title
+                </ArticleInfoTitle1><ArticleInfoAuthor1>By Some Person's Name</ArticleInfoAuthor1></ArticleInfo1>
+                <View1>
+                    <img src="https://www.history.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTk0OTA1OTY4ODg1MTE0Mzky/hero-crop-gettyimages-459534214.jpg" alt="Dr. King"></img>
+                    </View1>
+            <Rectangle1><LittleRectangle1/></Rectangle1>
             
-                <ArticleInfo2><ArticleInfoTitle2>This Article's Title
+
+            <ArticleInfo2><ArticleInfoTitle2>This Article's Title
                 </ArticleInfoTitle2><ArticleInfoAuthor2>By Some Person's Name</ArticleInfoAuthor2></ArticleInfo2>
+                <View2>
+                    <img src="https://www.history.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTk0OTA1OTY4ODg1MTE0Mzky/hero-crop-gettyimages-459534214.jpg" alt="Dr. King"></img>
+                </View2>
                 <Rectangle2><LittleRectangle2/></Rectangle2>
-
-
             </AllRectangles>
         </Container>
     );
