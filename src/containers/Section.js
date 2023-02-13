0088170bd5a8f1e50555fcab6ba2898a30e53027
Caroline <caroline.haoud@columbia.edu>
2023-02-12 22:23:16 -0500

    added all styles, need to fix title and add image

--- src/containers/Section.js
+++ src/containers/Section.js
@@ -4,155 +4,137 @@ import NavBar from '../components/NavBar';
 
 
 const Container = styled.div` // container for all components
-    height: 100vh;
     width: 100vw;
     background: linear-gradient(#5F193E, #3F132e, #1D132b); 
 `;
-const HeaderDiv = styled.div`
-    color: white;
-    font-family: EB Garamond;
-    font-size: 70px;
-    font-weight: 600;
-    line-height: 91px;
-    letter-spacing: 0em;
-    text-align: left;
+const AllRectangles = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 400px;
+  position: relative;
+  height: 50%;
+  padding: 110px 350px 28px 45px;
+  margin-right: 300px;
 `;
 
-const Header = styled.div`
-    font-family: 'EB Garamond';
-    font-style: normal;
-    font-weight: 600;
-    font-size: 70px;
-    line-height: 91px;
-    text-transform: capitalize;
+const Rectangle = styled.div`
+  height: 100%;
+  width: 100%;
+  margin: 15px;
 `;
 
-const AllArticlesBox = styled.div`
-    position: absolute;
-    height: 100%;
-    width: 100%;
-    left: 0px;
-    top: 0px;
-    border-radius: 0px;
-`;  
-
-const View1 = styled.img` 
-    height: 75%;
-    width: 50%;
-    left: 118.02099609375px;
-    top: 159px;
-    border-radius: 0px;
-    background-size: cover;
-    z-index: 1;
-`;
-
-const View2 = styled.img`
-    position: absolute;
-    left: 44.82%;
-    right: 8.61%;
-    top: 0%;
-    bottom: 23.08%;
-    z-index: 1;
-`;
-const ArticleInfo1 = styled.div` 
-    left: 50.06%;
-    right: 6.88%;
-    top: 12.93%;
-    bottom: 73.38%;
-    font-family: 'EB Garamond';
-    font-style: normal;
-    font-weight: 600;
-    font-size: 40px;
-    line-height: 52px;
-    text-transform: capitalize;
-    color: #FFFFFF;
-    z-index: 1;
-`;
-const ArticleInfo2 = styled.div`
-    height: 72px;
-    width: 393px;
-    left: 100px;
-    top: 40px;
-    font-family: 'EB Garamond';
-    font-style: normal;
-    font-weight: 600;
-    font-size: 40px;
-    line-height: 52px;
-    text-transform: capitalize;
-    color: #FFFFFF;
-    z-index: 1;
-`;
-
-const ArticleTitle = styled.div`
-    font-size: 1.5rem;
-`;
-
-const ArticleAuthor = styled.div`
-    font-size: 1.5rem;
-`;
-
-const BigBox1 = styled.div`
-    position: absolute;
-    background: #7A5470;
+const Rectangle1 = styled(Rectangle)`
+    background-color: #7A5470;
     height: 526px;
     width: 887px;
     left: 0px;
-    top: 600px;
+    top: 0px;
     border-radius: 0px;
-    left: 0%;
-    right: 0%;
-    top: 0%;
-    bottom: 0%;
-    z-index: 3;
+    margin: 10px;
+    margin-left: 100px;
 `;
 
-const LittleBox1= styled.div`
-    position: absolute;
+const LittleRectangle1 = styled.div`
     background: #907388;
     height: 293px;
     width: 356.8284912109375px;
-    left: 118.02099609375px;
+    left: 118.0205078125px;
     top: 159px;
     border-radius: 0px;
-    z-index: 2;
+    align-items: center;
+    margin-left: 150px;
+    margin-top: 100px;
+
 `;
 
-const BigBox2 = styled.div`
-    background: #5F415B;
-    height: 356px;
+
+const Rectangle2 = styled(Rectangle)`
+    background-color: #5F415B;
+    height: 325px;
     width: 683px;
-    left: 121px;
+    right: 300px;
     top: 34px;
     border-radius: 0px;
-    position: absolute;
-    left: 14.09%;
-    right: 6.4%;
-    top: 8.72%;
-    bottom: 0%;
-    z-index: 3;
+    margin: 10px;
+    margin-left: 225px;
+    justify-content: right;
 `;
-const LittleBox2= styled.div`
+
+const LittleRectangle2 = styled.div`
     background: #70586D;
     height: 293px;
-    width: 387px;
-    left: 472px;
-    top: 76px;
+    width: 356.8284912109375px;
+    left: 118.0205078125px;
+    top: 159px;
     border-radius: 0px;
-    position: absolute;
-    left: 54.95%;
-    right: 0%;
-    top: 19.49%;
-    bottom: 5.38%;
-    z-index: 2;
+    align-items: center;
+    margin-left: 400px;
+    /* margin-bottom: 1000px; */
+`;
 
+const ArticleInfo1= styled.div`
+    text-align: right;
+    color: white;
+    margin: 10px;
+    font-family: 'EB Garamond';
+    text-transform: capitalize;
+`;
+const ArticleInfoTitle1 = styled(ArticleInfo1)`
+    font-size: 30px;
+`;
+const ArticleInfoAuthor1 = styled(ArticleInfo1)`
+    margin: 10px;
+    color: white;
+    font-size: 15px;
+`;
+
+const ArticleInfo2= styled.div`
+    text-align: left;
+    color: white;
+    margin: 10px;
+    font-family: 'EB Garamond';
+    text-transform: capitalize;
+`;
+const ArticleInfoTitle2 = styled(ArticleInfo2)`
+    font-size: 30px;
+`;
+const ArticleInfoAuthor2 = styled(ArticleInfo2)`
+    margin: 10px;
+    color: white;
+    font-size: 15px;
 `;
 const Section = () => {
 
     return (
         <Container id = "section">
             <NavBar isSection/>
-            <HeaderDiv><Header>UNIVERSITY</Header></HeaderDiv>
-            <AllArticlesBox>
+            {/* <HeaderDiv><Header>UNIVERSITY</Header></HeaderDiv> */}
+            {/* <AllArticlesBox> */}
+            <AllRectangles>
+            <Rectangle1><ArticleInfo1><ArticleInfoTitle1>TITLE BLAH
+                </ArticleInfoTitle1><ArticleInfoAuthor1>BY BLAH BLAH</ArticleInfoAuthor1></ArticleInfo1><LittleRectangle1/></Rectangle1>
+
+
+                <Rectangle2><ArticleInfo2><ArticleInfoTitle2>TITLE BLAH
+                </ArticleInfoTitle2><ArticleInfoAuthor2>BY BLAH BLAH</ArticleInfoAuthor2></ArticleInfo2><LittleRectangle2/></Rectangle2>
+
+
+            <Rectangle1><ArticleInfo1><ArticleInfoTitle1>TITLE BLAH
+                </ArticleInfoTitle1><ArticleInfoAuthor1>BY BLAH BLAH</ArticleInfoAuthor1></ArticleInfo1><LittleRectangle1/></Rectangle1>
+           
+
+                <Rectangle2><ArticleInfo2><ArticleInfoTitle2>TITLE BLAH
+                </ArticleInfoTitle2><ArticleInfoAuthor2>BY BLAH BLAH</ArticleInfoAuthor2></ArticleInfo2><LittleRectangle2/></Rectangle2>
+            {/* <Rectangle1 />
+            <Rectangle2 />
+            <Rectangle1 />
+            <Rectangle2 />
+            <Rectangle1 />
+            <Rectangle2 /> */}
+            </AllRectangles>
+
                 {/* <ArticleInfo1>
                     <BigBox1>
                     <ArticleTitle>Title</ArticleTitle>
@@ -173,7 +155,7 @@ const Section = () => {
                     </View2>
                         <LittleBox2></LittleBox2></BigBox2>
                 </ArticleInfo2> */}
-            </AllArticlesBox>
+            {/* </AllArticlesBox> */}
         </Container>
     );
 };
