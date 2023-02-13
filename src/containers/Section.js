d5d8e7290ac1ea12f56cfcb4200bafd26b0b6c8b
Caroline <caroline.haoud@columbia.edu>
2023-02-13 18:09:50 -0500

    fixed article 1 box

--- src/containers/Section.js
+++ src/containers/Section.js
@@ -28,7 +28,6 @@ const Header = styled.div`
     
 `;
 
-
 const AllRectangles = styled.div`
   display: flex;
   flex-direction: column;
@@ -48,6 +47,7 @@ const Rectangle = styled.div`
 `;
 
 const Rectangle1 = styled(Rectangle)`
+    display: flex;
     background-color: #7A5470;
     height: 526px;
     width: 887px;
@@ -59,103 +59,71 @@ const Rectangle1 = styled(Rectangle)`
     z-index: 1;
 `;
 
-const LittleRectangle1 = styled.div`
-    background: #907388;
-    height: 293px;
-    width: 356.8284912109375px;
-    left: 118.0205078125px;
-    top: 159px;
-    border-radius: 0px;
-    align-items: center;
-    margin-left: 150px;
-    margin-top: 100px;
-    z-index: 2;
-`;
-
-const View1 = styled.image`
-    max-width:100%;
-    height:auto;
-    z-index: 100;
-    margin-right: 700px;
-`;
-
-const Rectangle2 = styled(Rectangle)`
-    background-color: #5F415B;
-    height: 325px;
-    width: 683px;
-    right: 300px;
-    top: 34px;
-    border-radius: 0px;
-    margin: 10px;
-    margin-left: 225px;
-    justify-content: right;
-    z-index: 1;
-`;
-
-const LittleRectangle2 = styled.div`
-    background: #70586D;
-    height: 293px;
-    width: 356.8284912109375px;
-    left: 118.0205078125px;
-    top: 159px;
-    border-radius: 0px;
-    align-items: center;
-    margin-left: 400px;
-    margin-top: 10px;
-    z-index: 2;
-`;
-
-const ArticleInfo1= styled.div`
-    text-align: right;
-    color: white;
-    font-family: 'EB Garamond';
-    text-transform: capitalize;
-    font-weight: 600;
+const Column1 = styled.div`
+    width: 50%;
+    background-color: #7A5470;
+    border: 2px solid black;
     position: relative;
-    z-index: 10;
-    margin-top: 100px;
-`;
-const ArticleInfoTitle1 = styled(ArticleInfo1)`
-    font-size: 40px;
-`;
-const ArticleInfoAuthor1 = styled(ArticleInfo1)`
-    color: white;
-    font-size: 20px;
+    border-radius: 0px;
+    border-color: #7A5470;
 `;
 
-const View2 = styled.image`
-    margin-left: 700px;
-    max-width:100%;
-    height:auto;
-    z-index: 100;
+const MainImage1 = styled.div`
+    height: 55%;
     position: absolute;
+    width: 350px;
+    background-position: center;
+    background-size: cover;
+    background-image: url(${props => props.src});
+    z-index: 2;
+    top: 25px;
+    left: 25px;
+    @media screen and (max-width: 768px) {
+        width: 10px;
+    } 
 `;
 
-const ArticleInfo2= styled.div`
-    text-align: left;
-    color: white;
+const OffSetRectangle1 = styled.div`
+    background: #907388;
+    height: 55%;
+    position: absolute;
+    width: 350px;
+    z-index: 1;
+    left: 48px;
+    top: 150px;
+    @media screen and (max-width: 768px) {
+        width: 10px;
+    } 
+`;
+
+const ArticleInfo1 = styled.div`
+    display: flex;
+    flex-direction: column;
     font-family: 'EB Garamond';
-    font-weight: 600;
     text-transform: capitalize;
-    
-    height: 72px;
-    width: 393px;
-    left: 0px;
-    top: 40px;
-    z-index: 10;
-    margin-right: 300px;
-    margin-top: 50px;
+    color: white;
+`;
+
+const ArticleTitle1= styled(ArticleInfo1)`
+    size: 100px;
+`;
+
+const ArticleAuthor1= styled(ArticleInfo1)`
+    size: 16px;
 
 `;
-const ArticleInfoTitle2 = styled(ArticleInfo2)`
-    font-size: 40px;
-    z-index: 10;
+
+const Rectangle2 = style.div`
+    background: #5F415B;
 `;
-const ArticleInfoAuthor2 = styled(ArticleInfo2)`
-    color: white;
-    font-size: 20px;
-    z-index: 10;
+
+const OffSetRectangle2 = style.div`
+    background: #70586D;
 `;
+
+
+
+
 const Section = () => {
 
     return (
@@ -163,40 +131,38 @@ const Section = () => {
             <NavBar isSection/>
             <HeaderDiv><Header>UNIVERSITY</Header></HeaderDiv>
             <AllRectangles>
-            <ArticleInfo1>
-                <ArticleInfoTitle1>This Article's Title</ArticleInfoTitle1>
-                <ArticleInfoAuthor1>By Some Person's Name</ArticleInfoAuthor1>
-            </ArticleInfo1>
-                <View1>
-                    <img src="https://www.history.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTk0OTA1OTY4ODg1MTE0Mzky/hero-crop-gettyimages-459534214.jpg" alt="Dr. King"></img>
-                </View1>
+
             <Rectangle1>
-                <LittleRectangle1>
-                </LittleRectangle1>
+                <Column1>
+                    <MainImage1 src="https://www.history.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTk0OTA1OTY4ODg1MTE0Mzky/hero-crop-gettyimages-459534214.jpg"></MainImage1>
+                    <OffSetRectangle1></OffSetRectangle1>
+                </Column1>
+                <Column1>
+                    <ArticleInfo1> 
+                        <ArticleTitle1>Article Title </ArticleTitle1>
+                        <ArticleAuthor1>By Some Person's Name </ArticleAuthor1>
+                    </ArticleInfo1>
+                </Column1>
             </Rectangle1>
 
-
-            <ArticleInfo2>
-                <ArticleInfoTitle2>This Article's Title</ArticleInfoTitle2>
-                <ArticleInfoAuthor2>By Some Person's Name</ArticleInfoAuthor2>
-            </ArticleInfo2>
-                <View2>
-                    <img src="https://www.history.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTk0OTA1OTY4ODg1MTE0Mzky/hero-crop-gettyimages-459534214.jpg" alt="Dr. King"></img>
-                </View2>
-                <Rectangle2>
-                    <LittleRectangle2>
-                    </LittleRectangle2>
-                </Rectangle2>
+            <Rectangle1>
+                <Column1>
+                    <MainImage1 src="https://upload.wikimedia.org/wikipedia/commons/0/05/Martin_Luther_King%2C_Jr..jpg"></MainImage1>
+                    <OffSetRectangle1></OffSetRectangle1>
+                </Column1>
+                <Column1>
+                    <ArticleInfo1> 
+                        <ArticleTitle1>Article Title </ArticleTitle1>
+                        <ArticleAuthor1>By Some Person's Name </ArticleAuthor1>
+                    </ArticleInfo1>
+                </Column1>
+            </Rectangle1>
             </AllRectangles>
         </Container>
     );
 };
 
-@media (max-width: 768px) {
-    .container {
-      width: 100%;
-    }
-  }
+
   
 export default Section;
 
