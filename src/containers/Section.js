263ca3d8f04b93e9be23bf2887b45cc27522baa6
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2023-02-15 17:18:56 -0500

    Cleaned up styling for section page component

--- src/containers/Section.js
+++ src/containers/Section.js
@@ -5,8 +5,7 @@ import MobileNavBar from '../components/MobileNavBar';
 import LeftArticle from '../components/LeftArticle';
 import RightArticle from '../components/RightArticle';
 
-const Container = styled.div` // container for all components
-    width: 100%;
+const Container = styled.div`
     min-height: 100vh;
     background: linear-gradient(180deg, #945F80 0%, #10071D 100%), #3F132E; 
 `;
@@ -16,45 +15,28 @@ const HeaderDiv = styled.div`
     font-family: EB Garamond;
     font-size: 70px;
     font-weight: 600;
-    line-height: 91px;
-    letter-spacing: 0em;
     text-align: left;
+    padding: 2rem 4rem;
     display: flex;
-
-    @media screen and (max-width: 768px) {
-        justify-content: center;
-        align-items: center;
-    }
-`;
-
-const Header = styled.div`
-    font-family: 'EB Garamond';
-    font-style: normal;
-    font-weight: 600;
-    font-size: 70px;
-    line-height: 91px;
     text-transform: capitalize;
-    justify-content: center;
+
     @media screen and (max-width: 768px) {
+        margin: 0rem auto;
         font-size: 40px;
     }
 `;
 
 const ArticlesContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 400px;
-  position: relative;
-  height: 100%;
-  padding-top: 25px;
-    @media screen and (max-width: 768px) {
-        margin: 0px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    
+    padding-top: 25px;
+        @media screen and (max-width: 768px) {
+            margin: 0px;
+            display: flex;
+            align-items: center;
+        }
 `;
 
 const Section = ({ header, articles }) => {
@@ -63,7 +45,7 @@ const Section = ({ header, articles }) => {
             <NavBar isSection />
             <MobileNavBar />
             <HeaderDiv>
-                <Header>{header}</Header>
+                {header}
             </HeaderDiv>
             <ArticlesContainer>
                 {articles.map((article, index) => (
